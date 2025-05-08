3fffd0fd184465f9059a9dcf1420c4baaf252e9f
Vladislav Alexeyev <vladislav.j.alexeyev@gmail.com>
2025-05-08 10:52:38 +0200

    Update return DTO

--- src/GL/ReturnManagement/DTO/Request/Return/Update.php
+++ src/GL/ReturnManagement/DTO/Request/Return/Update.php
@@ -4,16 +4,66 @@ namespace swiatprzesylek\GL\ReturnManagement\DTO\Request\Return;
 
 class Update
 {
-    protected string $outboundId;
+    protected ?string $localId = null;
+    protected ?string $outboundLocalId = null;
+    protected ?string $customerRef = null;
 
-    public function getOutboundId(): string
+    protected ?string $operator = null;
+
+    protected ?string $note = null;
+
+    public function getLocalId(): ?string
     {
-        return $this->outboundId;
+        return $this->localId;
     }
 
-    public function setOutboundId(string $outboundId): Update
+    public function setLocalId(?string $localId): Update
     {
-        $this->outboundId = $outboundId;
+        $this->localId = $localId;
+        return $this;
+    }
+
+    public function getOutboundLocalId(): ?string
+    {
+        return $this->outboundLocalId;
+    }
+
+    public function setOutboundLocalId(?string $outboundLocalId): Update
+    {
+        $this->outboundLocalId = $outboundLocalId;
+        return $this;
+    }
+
+    public function getCustomerRef(): ?string
+    {
+        return $this->customerRef;
+    }
+
+    public function setCustomerRef(?string $customerRef): Update
+    {
+        $this->customerRef = $customerRef;
+        return $this;
+    }
+
+    public function getOperator(): ?string
+    {
+        return $this->operator;
+    }
+
+    public function setOperator(?string $operator): Update
+    {
+        $this->operator = $operator;
+        return $this;
+    }
+
+    public function getNote(): ?string
+    {
+        return $this->note;
+    }
+
+    public function setNote(?string $note): Update
+    {
+        $this->note = $note;
         return $this;
     }
 }

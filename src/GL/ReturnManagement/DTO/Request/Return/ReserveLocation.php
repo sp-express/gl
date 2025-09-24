f677a062b72738b3dcaebc50a07ce0bd8a9671b8
Vladislav Alexeyev <vladislav.j.alexeyev@gmail.com>
2025-09-24 10:58:47 +0200

    dev

--- src/GL/ReturnManagement/DTO/Request/Return/ReserveLocation.php
+++ src/GL/ReturnManagement/DTO/Request/Return/ReserveLocation.php
@@ -4,33 +4,8 @@ namespace swiatprzesylek\GL\ReturnManagement\DTO\Request\Return;
 
 class ReserveLocation
 {
-    protected int $warehouseId;
-
-    protected string $subinventoryInternalId;
-
     protected string $returnLocalId;
 
-    public function getWarehouseId(): int
-    {
-        return $this->warehouseId;
-    }
-
-    public function setWarehouseId(int $warehouseId): ReserveLocation
-    {
-        $this->warehouseId = $warehouseId;
-        return $this;
-    }
-
-    public function getSubinventoryInternalId(): string
-    {
-        return $this->subinventoryInternalId;
-    }
-
-    public function setSubinventoryInternalId(string $subinventoryInternalId): ReserveLocation
-    {
-        $this->subinventoryInternalId = $subinventoryInternalId;
-        return $this;
-    }
 
     public function getReturnLocalId(): string
     {

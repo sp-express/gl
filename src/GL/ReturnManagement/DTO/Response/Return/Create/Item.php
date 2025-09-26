339be7398317b1897cd619aa8f3c9733767a79eb
Vladislav Alexeyev <vladislav.j.alexeyev@gmail.com>
2025-09-26 10:55:01 +0200

    Add OA attributes

--- src/GL/ReturnManagement/DTO/Response/Return/Create/Item.php
+++ src/GL/ReturnManagement/DTO/Response/Return/Create/Item.php
@@ -1,11 +1,30 @@
 <?php
 
 namespace swiatprzesylek\GL\ReturnManagement\DTO\Response\Return\Create;
-
+use OpenApi\Attributes as OA;
 class Item
 {
-    protected string $localId;
-    protected array $items;
+    #[OA\Property(
+        description: 'Local identifier',
+        example: 'R250926/999/008/002'
+    )]
+    public string $localId;
+
+    #[OA\Property(
+        description: 'Map of product IDs to arrays of item UUIDs',
+        type: 'object',
+        example: [
+            '33969' => [
+                '44177d85-5a9b-465f-879b-46881a77b697',
+                '8c555273-028f-47e5-85c6-5aed052fdb0d',
+            ],
+        ],
+        additionalProperties: new OA\AdditionalProperties(
+            type: 'array',
+            items: new OA\Items(type: 'string', format: 'uuid')
+        )
+    )]
+    public array $items;
 
     public function getLocalId(): string
     {

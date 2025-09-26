798d7e67c0c6d3f8eea8c5ceb8da5d581b32eaf3
Vladislav Alexeyev <vladislav.j.alexeyev@gmail.com>
2025-09-26 11:37:11 +0200

    Add schemas

--- src/GL/ReturnManagement/DTO/Response/Return/Item.php
+++ src/GL/ReturnManagement/DTO/Response/Return/Item.php
@@ -3,6 +3,10 @@
 namespace swiatprzesylek\GL\ReturnManagement\DTO\Response\Return;
 use OpenApi\Attributes as OA;
 
+#[OA\Schema(
+    schema: 'Item',
+    description: 'Return item information'
+)]
 class Item
 {
     #[OA\Property(
